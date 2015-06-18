60378bd9be387aefdc194ca802fad98814e4eb6a
David Windell <david@outeredgeuk.com>
2015-06-18 22:39:52 +0100

    Add bash autocomplete support

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ INSTALL_PATH=/usr/local/dredger
 sudo mkdir -p $INSTALL_PATH
 sudo wget https://github.com/outeredge/dredger/archive/master.tar.gz -qO - | sudo tar -zxf - -C $INSTALL_PATH --strip=1
 sudo ln -sf $INSTALL_PATH/dredger /usr/local/bin/dredger
+sudo ln -sf $INSTALL_PATH/autocomplete.sh /etc/bash_completion.d/dredger
 
 echo "Dredger installed successfully!"
