f85e2133e0e2a7d4e9450c271b07d9fb27e41f1d
David Windell <david@outeredgeuk.com>
2017-04-26 20:45:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 
 INSTALL_PATH=/usr/local/dredger
 
-mkdir -p $INSTALL_PATH
+mkdir -p $INSTALL_PATH /etc/bash_completion.d
 wget https://github.com/outeredge/dredger/archive/master.tar.gz -qO - | tar -zxf - -C $INSTALL_PATH --strip=1
 ln -sf $INSTALL_PATH/dredger /usr/local/bin/dredger
 ln -sf $INSTALL_PATH/autocomplete.sh /etc/bash_completion.d/dredger
