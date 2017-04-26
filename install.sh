345321fb8f3b14cda1079386aaaf9f5d3d5906d4
David Windell <david@outeredgeuk.com>
2017-04-26 13:45:37 +0100

    Remove sudo for alpine support
    User should/can now install with root or sudo

--- install.sh
+++ install.sh
@@ -3,12 +3,9 @@ set -e
 
 INSTALL_PATH=/usr/local/dredger
 
-sudo apt-get install make
-
-sudo mkdir -p $INSTALL_PATH
-sudo wget https://github.com/outeredge/dredger/archive/master.tar.gz -qO - | sudo tar -zxf - -C $INSTALL_PATH --strip=1
-sudo ln -sf $INSTALL_PATH/dredger /usr/local/bin/dredger
-sudo ln -sf $INSTALL_PATH/git-timestamps.sh /usr/local/bin/git-timestamps
-sudo ln -sf $INSTALL_PATH/autocomplete.sh /etc/bash_completion.d/dredger
+mkdir -p $INSTALL_PATH
+wget https://github.com/outeredge/dredger/archive/master.tar.gz -qO - | tar -zxf - -C $INSTALL_PATH --strip=1
+ln -sf $INSTALL_PATH/dredger /usr/local/bin/dredger
+ln -sf $INSTALL_PATH/autocomplete.sh /etc/bash_completion.d/dredger
 
 echo "Dredger installed successfully!"
