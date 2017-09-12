78ad54cce40105775820e2866f8823a08961265b
David Windell <david@outeredgeuk.com>
2017-09-12 13:33:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ set -e
 
 INSTALL_PATH=/usr/local/dredger
 
+if [ $UID != 0 ]; then
+    echo "You don't have sufficient privileges to run this script, try `sudo`."
+    exit 1
+fi
+
 mkdir -p $INSTALL_PATH /etc/bash_completion.d
 wget https://github.com/outeredge/dredger/archive/master.tar.gz -qO - | tar -zxf - -C $INSTALL_PATH --strip=1
 ln -sf $INSTALL_PATH/dredger /usr/local/bin/dredger
